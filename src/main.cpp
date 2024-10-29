234c5e10dfa54461e2e6a40e3a6c58a90df159ad
Srushti24 <srushti.desai@gmail.com>
2024-10-29 13:39:34 -0400

    basic implementation of vector with test cases covering everything

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
-#include "vector.hpp"
+#include "vector_test.cpp"
 #include <iostream>
 
 int main(int argc, const char* argv[]) {
-    Vector<int> temp;
-   // std::cout << temp.m_array << std::endl;
-    temp.push_back(10);
-    temp.push_back(2);
-    // std::cout << temp.m_array << std::endl;
-    for(size_t i=0; i<2; i++)
-    {
-        std::cout << temp[i] << std::endl;
-    }
+    testVectorPushPopSize();
+    testVectorResizeValueOptClear();
+    testVectorCopyConstrCopyAssignOpt();
+    testVectorMoveConstructor();
+    testVectorMoveAssignOpt();
     return 0;
 }
